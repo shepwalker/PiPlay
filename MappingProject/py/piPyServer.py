2312a322e3475ad86d3918356411aa3083d26cee
Shep.Walker <shep.walker@live.com>
2014-03-15 12:01:10 -0700

    Initial working commit

---
+++ MappingProject/py/piPyServer.py
@@ -0,0 +1,46 @@
+﻿from flask import Flask, jsonify, render_template, request
+from flask.ext.socketio import SocketIO, emit
+import RPi.GPIO as GPIO
+from time import sleep
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+pinDict = {'w' : 25, 'a' : 24, 's' : 24, 'd' : 22}
+actDict = {'down' : 1, 'up' : 0}
+           
+@app.route("/hitme", methods = ["POST"])   
+def hitme():
+    if request.method == "POST":
+        instructions = request.json['payload']
+        for k in instructions:
+            setGPIOpin(k, instructions[k])  
+    return ""
+
+
+@app.route("/")
+def index():
+    return render_template('template.htm')
+
+def setGPIOpin(instruct, value):
+    print(str(pinDict[instruct]) + " set to " + str(1 if value else 0))
+    GPIO.output(pinDict[instruct], 1 if value else 0)
+
+@socketio.on('connect', namespace='/hitmesock')
+def hitme_connect():
+    emit('response', {'data' : 'Connected'})
+
+@socketio.on('disconnect', namespace='/hitmesock')
+def hitme_disconnect():
+    print('Client disconnected')
+
+if __name__ == "__main__":
+    GPIO.setmode(GPIO.BCM)
+    for i in pinDict.values():
+        print("registering GPIO number " + str(i))
+        GPIO.setup(i, GPIO.OUT)
+        GPIO.output(i, 0)
+    app.debug = True
+    #app.run('0.0.0.0', 5001)    
+    socketio.run(app, '0.0.0.0', 5001)
+
---
+++ MappingProject/py/static/driver.js
@@ -0,0 +1,74 @@
+﻿
+///<reference path="jquery-1.10.2.js"/>
+var keytrack = new Object();
+keytrack["w"] = false;
+keytrack["a"] = false;
+keytrack["s"] = false;
+keytrack["d"] = false;
+function changeKey(location, value) {
+    if (keytrack[location] != value) {
+        keytrack[location] = value;
+        $(keytrack).trigger("moveChange");
+    }
+}
+$(function () {
+    $('body').keydown(function (event) {
+        switch (event.which) {
+            //A
+            case 65:
+                changeKey("a", true);
+                break;
+                //D
+            case 68:
+                changeKey("d", true);
+                break;
+                //S
+            case 83:
+                changeKey("s", true);
+                break;
+                //W
+            case 87:
+                changeKey("w", true);
+                break;
+            default:
+                payload = null;
+                break;
+        }
+    }).keyup(function (event) {
+        switch (event.which) {
+            //A
+            case 65:
+                changeKey("a", false);
+                break;
+                //D
+            case 68:
+                changeKey("d", false);
+                break;
+                //S
+            case 83:
+                changeKey("s", false);
+                break;
+                //W
+            case 87:
+                changeKey("w", false);
+                break;
+            default:
+                break;
+        }
+    });//TODO: Doesn't look like "change" works for simple objects. Try using hidden objects or obj.trigger/obj.triggerhandler  DONE DONE
+    $(keytrack).bind("moveChange", function () {
+        data = new Object();
+        path = '/hitme'
+        data["payload"] = keytrack;
+        data["user"] = "bleh";
+        $.ajax({
+            type: "POST",
+            url: $SCRIPT_ROOT + path,
+            data: JSON.stringify(data, null, '\t'),
+            contentType: 'application/json;charset=UTF-8',
+            success: function (result) {
+                console.log("successful post");
+            }
+        });
+    })
+});
