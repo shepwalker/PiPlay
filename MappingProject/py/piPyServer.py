5ec1b54423a39a5f6c69e6f0457e2f82432a36b7
Shep.Walker <shep.walker@live.com>
2014-03-15 14:09:59 -0700

    Switched to using SocketIO for server-client interaction instead of ajax calls.

--- MappingProject/py/piPyServer.py
+++ MappingProject/py/piPyServer.py
@@ -9,6 +9,7 @@ socketio = SocketIO(app)
 pinDict = {'w' : 25, 'a' : 24, 's' : 24, 'd' : 22}
 actDict = {'down' : 1, 'up' : 0}
            
+#Plain 'ol AJAX
 @app.route("/hitme", methods = ["POST"])   
 def hitme():
     if request.method == "POST":
@@ -26,21 +27,33 @@ def setGPIOpin(instruct, value):
     print(str(pinDict[instruct]) + " set to " + str(1 if value else 0))
     GPIO.output(pinDict[instruct], 1 if value else 0)
 
+#SOCKET WORK
 @socketio.on('connect', namespace='/hitmesock')
 def hitme_connect():
-    emit('response', {'data' : 'Connected'})
+    print("client connected")
+    emit('response', {'data' : 'Connected to remote server. Control given'})
 
 @socketio.on('disconnect', namespace='/hitmesock')
 def hitme_disconnect():
     print('Client disconnected')
+    for pin in pinDict:
+        GPIO.output(pinDict[pin], 0)
 
-if __name__ == "__main__":
+@socketio.on('changemove', namespace='/hitmesock')
+def changemove(move):
+    print("ChangeMove hit")
+    instructions = move['payload']
+    for k in instructions:
+        setGPIOpin(k, instructions[k])  
+
+
+if __name__ == "__main__": 
     GPIO.setmode(GPIO.BCM)
     for i in pinDict.values():
         print("registering GPIO number " + str(i))
         GPIO.setup(i, GPIO.OUT)
         GPIO.output(i, 0)
     app.debug = True
-    #app.run('0.0.0.0', 5001)    
+    #app.run('0.0.0.0', 5001)
     socketio.run(app, '0.0.0.0', 5001)
 
--- MappingProject/py/static/driver.js
+++ MappingProject/py/static/driver.js
@@ -1,62 +1,55 @@
-﻿
-///<reference path="jquery-1.10.2.js"/>
+﻿///<reference path="jquery-1.10.2.js"/>
+///<reference path="http:////cdnjs.cloudflare.com/ajax/libs/socket.io/0.9.16/socket.io.min.js/>
 var keytrack = new Object();
 keytrack["w"] = false;
 keytrack["a"] = false;
 keytrack["s"] = false;
 keytrack["d"] = false;
+
 function changeKey(location, value) {
     if (keytrack[location] != value) {
         keytrack[location] = value;
         $(keytrack).trigger("moveChange");
     }
 }
+
+function pickKey(which, value) {
+    switch (which) {
+        //A
+        case 65:
+            changeKey("a", value);
+            break;
+            //D
+        case 68:
+            changeKey("d", value);
+            break;
+            //S
+        case 83:
+            changeKey("s", value);
+            break;
+            //W
+        case 87:
+            changeKey("w", value);
+            break;
+        default:
+            payload = null;
+            break;
+    }
+}
+
+
 $(function () {
+    var socket = io.connect($SCRIPT_ROOT + "/hitmesock"); 
+
+
+
+
     $('body').keydown(function (event) {
-        switch (event.which) {
-            //A
-            case 65:
-                changeKey("a", true);
-                break;
-                //D
-            case 68:
-                changeKey("d", true);
-                break;
-                //S
-            case 83:
-                changeKey("s", true);
-                break;
-                //W
-            case 87:
-                changeKey("w", true);
-                break;
-            default:
-                payload = null;
-                break;
-        }
+        pickKey(event.which, true);
     }).keyup(function (event) {
-        switch (event.which) {
-            //A
-            case 65:
-                changeKey("a", false);
-                break;
-                //D
-            case 68:
-                changeKey("d", false);
-                break;
-                //S
-            case 83:
-                changeKey("s", false);
-                break;
-                //W
-            case 87:
-                changeKey("w", false);
-                break;
-            default:
-                break;
-        }
+        pickKey(event.which, false);
     });//TODO: Doesn't look like "change" works for simple objects. Try using hidden objects or obj.trigger/obj.triggerhandler  DONE DONE
-    $(keytrack).bind("moveChange", function () {
+  /*$(keytrack).bind("moveChange", function () {
         data = new Object();
         path = '/hitme'
         data["payload"] = keytrack;
@@ -70,5 +63,8 @@ $(function () {
                 console.log("successful post");
             }
         });
-    })
+    });*/
+    $(keytrack).bind("moveChange", function () {
+        socket.emit('changemove', { payload: keytrack });
+    });
 });
